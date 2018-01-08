43ea6a0b495950aa4c83a232cc814e959f67f186
maxiking203 <Maximilian.Sachmann@gmx.de>
2018-01-08 09:23:04 +0100

    Second try Rotate Left remove

--- test/testAVLTree.cpp
+++ test/testAVLTree.cpp
@@ -700,12 +700,14 @@ TEST(AvlTreeTest, Rem2){
 
 TEST(AvlTreeTest, Remove_LeftRotete){
     AVLTree tree;
-    tree.insert(4);
-    tree.insert(3);
-    tree.insert(6);
-    tree.insert(7);
+    tree.insert(10);
     tree.insert(5);
-    tree.remove(3);
-    EXPECT_FALSE(tree.search(3));
+    tree.insert(12);
+    tree.insert(11);
+    tree.insert(2);
+    tree.insert(13);
+    tree.insert(14);
+    tree.remove(2);
+    EXPECT_FALSE(tree.search(2));
 }
 
